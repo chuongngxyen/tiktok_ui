a8c0700160e33bf5b6419351f8f66861b8cd81af
chuongnguyen-ne <nnbcfang@gmail.com>
2023-02-01 15:26:14 +0700

    Khung Layout

--- src/component/Layout/DefaultLayout/index.js
+++ src/component/Layout/DefaultLayout/index.js
@@ -1,14 +1,22 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import styles from "./DefaultLayout.module.scss"
+import classNames from "classnames/bind";
 
+
+const cx = classNames.bind(styles)
 function DefaultLayout({ children }) {
-    return <div>
-        <Header />
-        <div className="container">
-            <Sidebar />
-            <div className="content">{children}</div>
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx("content")}>
+                    {children}
+                </div>
+            </div>
         </div>
-    </div>;
+    )
 }
 
 export default DefaultLayout;
--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,5 +1,16 @@
+import styles from "./Header.module.scss"
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles)
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('content')}>
+                {/* {logo} */}
+                {/* {content} */}
+            </div>
+        </div>
+    );
 }
 
 export default Header;
